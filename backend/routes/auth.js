c5609acab640e25d102a81f722543d3aa0b5c7a7
zeeshan-memon <52953436+zeeshan-memon@users.noreply.github.com>
2021-11-23 17:37:40 +0500

    lecture 49

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,26 +1,42 @@
-const express = require('express');
-const router =  express.Router();
-const User = require('../models/Users');
-const { body, validationResult } = require('express-validator');
-router.post('/',  [
-    body('email', 'Invalid Email').isEmail(),
-  // password must be at least 5 chars long
-  body('password', 'Password length must be greater than 5').isLength({ min: 5 }),
-  body('name', 'length must be greater than 5').isLength({ min: 5 }),
-], async(req, res)=>{
- 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(200).json({ errors: errors.array() });
-    }
+const express = require("express");
+const router = express.Router();
+const User = require("../models/Users");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-    const existingUser = await User.findOne({email:req.body.email});
-    if(existingUser){
-        return res.status(200).json({message:"User already exists"})
-    }
-    const user = await User.create(req.body);
-    return res.status(200).json({message:user})
 
-})
+router.post(
+  "/",
+  [
+    body("email", "Invalid Email").isEmail(),
+    // password must be at least 5 chars long
+    body("password", "Password length must be greater than 5").isLength({
+      min: 5,
+    }),
+    body("name", "length must be greater than 5").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(200).json({ errors: errors.array() });
+      }
+
+      const existingUser = await User.findOne({ email: req.body.email });
+      if (existingUser) {
+        return res.status(200).json({ message: "User already exists" });
+      }
+      const salt = await bcrypt.genSalt(10)
+      req.body.password = await bcrypt.hash(req.body.password, salt)
+      const user = await User.create(req.body);
+      const token = jwt.sign({ message: user }, 'shhhhh');
+      return res.status(200).json({token});
+    } catch (error) {
+      console.log("error", error);
+      return res.status(200).json({ error: error });
+    }
+  }
+);
 
 module.exports = router;
